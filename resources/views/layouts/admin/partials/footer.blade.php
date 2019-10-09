cdd27eda01d875c68bd34364473601b20a05f5fe
Bhushan Puranik <bhushan.puranik@pivotroots.com>
2019-10-09 18:28:58 +0530

    changes

--- resources/views/layouts/admin/partials/footer.blade.php
+++ resources/views/layouts/admin/partials/footer.blade.php
@@ -3,6 +3,6 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.4.0
     </div>
-    <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; {{date("Y")}} <a href="https://adminlte.io">Bhushan Puranik</a>.</strong> All rights
     reserved.
   </footer>
--- resources/views/layouts/admin/partials/side-menu-bar.blade.php
+++ resources/views/layouts/admin/partials/side-menu-bar.blade.php
@@ -5,10 +5,10 @@
     <!-- Sidebar user panel -->
     <div class="user-panel">
       <div class="pull-left image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        <img src="{{asset('admin/dist/img/user2-160x160.jpg')}}" class="img-circle" alt="User Image">
       </div>
       <div class="pull-left info">
-        <p>Alexander Pierce</p>
+        <p>Admin</p>
         <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
       </div>
     </div>
@@ -26,7 +26,8 @@
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu" data-widget="tree">
       <li class="header">MAIN NAVIGATION</li>
-      <li class="active treeview">
+       <li><a href=""><i class="fa fa-circle-o text-red"></i> <span>Dashboard</span></a></li>
+      <!-- <li class="active treeview">
         <a href="#">
           <i class="fa fa-dashboard"></i> <span>Dashboard</span>
           <span class="pull-right-container">
@@ -34,11 +35,10 @@
           </span>
         </a>
         <ul class="treeview-menu">
-          <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-          <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+          <li class="active"><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard</a></li>
         </ul>
-      </li>
-      <li class="treeview">
+      </li> -->
+      <!-- <li class="treeview">
         <a href="#">
           <i class="fa fa-files-o"></i>
           <span>Layout Options</span>
@@ -188,12 +188,12 @@
           </li>
           <li><a href="#"><i class="fa fa-circle-o"></i> Level One</a></li>
         </ul>
-      </li>
-      <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
+      </li> -->
+     <!--  <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
       <li class="header">LABELS</li>
       <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
       <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-      <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
+      <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li> -->
     </ul>
   </section>
   <!-- /.sidebar -->
